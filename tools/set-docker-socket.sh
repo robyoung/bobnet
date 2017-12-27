1a852d4b4d5b19e39194429c7ff6f9b82d0c3fc9
Rob Young <rob@robyoung.digital>
2017-12-27 23:36:12 +0000

    Add better errors to docker socket script

--- tools/set-docker-socket.sh
+++ tools/set-docker-socket.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
-set -e
-
 docker_socket=/tmp/bobnet-docker.sock
 control_socket=/tmp/bobnet-ssh.sock
+write_socket=0
 
 if [ ! -f $control_socket ]; then
   ssh -nNTf -MS $control_socket -L $docker_socket:/var/run/docker.sock $BOBNET_SWARM_MANAGER
+  if [ $? -eq 0 ]; then
+    write_socket=1
+  else
+    >&2 echo "Failed to ssh to swarm manager"
+  fi
+else
+  >&2 echo "Control socket already exists, you may need to close the socket"
 fi
 
-export DOCKER_HOST=unix://$docker_socket
+if [ $write_socket -eq 1 ]; then
+  export DOCKER_HOST=unix://$docker_socket
+fi
